595820c1699e82e26ee529620d87167d9f18fa01
Ana Julia Bittencourt <anajuliabit@gmail.com>
2020-08-17 12:27:41 -0300

    feat: add random_state

--- build/lib/my_custom_sklearn_transforms/sklearn_transformers.py
+++ build/lib/my_custom_sklearn_transforms/sklearn_transformers.py
@@ -19,25 +19,11 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return data.drop(labels=self.columns, axis='columns')
 
 
-class SetIndex(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.set_index(self.columns, inplace=True)
-
-
 class SmoteResample(object):
     def __init__(self):
         pass
 
     def fit(self, X, y):
-        X_resampled, y_resampled = SMOTE().fit_resample(X, y)
+        X_resampled, y_resampled = SMOTE(random_state=42).fit_resample(X, y)
         X_resampled = pd.DataFrame(X_resampled, columns=X.columns)
         return X_resampled, y_resampled
