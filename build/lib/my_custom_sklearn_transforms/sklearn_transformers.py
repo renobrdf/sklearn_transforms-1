6c64e6e3f43b3644241698efbfed8528ff21a563
Ana Julia Bittencourt <anajuliabit@gmail.com>
2020-08-17 13:25:34 -0300

    feat: add sampling strategy to smote resample

--- build/lib/my_custom_sklearn_transforms/sklearn_transformers.py
+++ build/lib/my_custom_sklearn_transforms/sklearn_transformers.py
@@ -24,6 +24,7 @@ class SmoteResample(object):
         pass
 
     def fit(self, X, y):
-        X_resampled, y_resampled = SMOTE(random_state=42).fit_resample(X, y)
+        X_resampled, y_resampled = SMOTE(
+            sampling_strategy='all').fit_resample(X, y)
         X_resampled = pd.DataFrame(X_resampled, columns=X.columns)
         return X_resampled, y_resampled
