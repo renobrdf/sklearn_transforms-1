d0494ab377527643b84bc026d9ae79b1dbb4548d
Ana Julia Bittencourt <anajuliabit@gmail.com>
2020-08-17 15:57:52 -0300

    feat: setIndex

--- build/lib/my_custom_sklearn_transforms/sklearn_transformers.py
+++ build/lib/my_custom_sklearn_transforms/sklearn_transformers.py
@@ -2,8 +2,6 @@ import pandas as pd
 from imblearn.over_sampling import SMOTE
 from sklearn.base import BaseEstimator, TransformerMixin
 
-# All sklearn Transforms must have the `transform` and `fit` methods
-
 
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -17,6 +15,20 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class SetIndex(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data.set_index(self.columns, inplace=True)
 
 
 class SmoteResample(object):
